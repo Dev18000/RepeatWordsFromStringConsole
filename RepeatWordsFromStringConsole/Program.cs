b8274b024c119e527e2193f5f56898e6fa3e5315
farida.seyidzade <farida.seyidzade@gmail.com>
2023-09-03 11:00:20 +0200

    Добавьте файлы проекта.

---
+++ RepeatWordsFromStringConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿string path = "C:/text-data.txt";
+
+using (StreamReader streamReaderForRead = File.OpenText(path))
+{
+    if (streamReaderForRead != null)
+    {
+        string streamTemps = "";
+        while ((streamTemps = streamReaderForRead.ReadLine()) != null)
+        {
+            var stringBase = string.Join("\n", streamTemps);
+            var stringBaseRepeat = new String('+', 3).Replace("+", $"{stringBase} ");
+
+            Console.Write($" \n{stringBase} -> {stringBaseRepeat}");
+        }
+    }
+}
